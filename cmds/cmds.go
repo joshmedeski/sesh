ce5592a9dacd3d2ed8df87962c065994e6de6825
alihammad-gist <syedalihammadshah@googlemail.com>
2023-12-29 06:00:18 +0500

    added fzf integration to allow choosing a session
    Created a new cli.Command to execute fzf. Also refactored out the
    shared logic that gathers list of sessions based on user preference
    into its own function.

--- cmds/cmds.go
+++ cmds/cmds.go
@@ -5,6 +5,7 @@ import (
 	"joshmedeski/sesh/tmux"
 	"joshmedeski/sesh/zoxide"
 	"os"
+	"os/exec"
 	"strings"
 
 	"github.com/urfave/cli/v2"
@@ -29,29 +30,78 @@ func ListSessions() *cli.Command {
 			},
 		},
 		Action: func(cCtx *cli.Context) error {
-			var sessions []string
-			hasFlags := cCtx.Bool("tmux") || cCtx.Bool("zoxide")
-
-			if !hasFlags || cCtx.Bool("tmux") {
-				tmuxSessions, err := tmux.Sessions()
-				if err != nil {
-					fmt.Println("Error:", err)
-					os.Exit(1)
-				}
-				sessions = append(sessions, tmuxSessions...)
-			}
-
-			if !hasFlags || cCtx.Bool("zoxide") {
-				dirs, err := zoxide.Dirs()
-				if err != nil {
-					fmt.Println("Error:", err)
-					os.Exit(1)
-				}
-				sessions = append(sessions, dirs...)
-			}
-
-			fmt.Println(strings.Join(sessions, "\n"))
+			fmt.Println(
+				strings.Join(requestedSessions(cCtx), "\n"),
+			)
 			return nil
 		},
 	}
+}
+
+func ChooseSession() *cli.Command {
+	return &cli.Command{
+		Name:                   "choose",
+		Aliases:                []string{"c"},
+		Usage:                  "Select session",
+		UseShortOptionHandling: true,
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:    "tmux",
+				Aliases: []string{"t"},
+				Usage:   "show tmux sessions",
+			},
+			&cli.BoolFlag{
+				Name:    "zoxide",
+				Aliases: []string{"z"},
+				Usage:   "show zoxide results",
+			},
+		},
+		Action: func(cCtx *cli.Context) error {
+			cmd := exec.Command("fzf")
+			stdin, err := cmd.StdinPipe()
+			if err != nil {
+				return err
+			}
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			err = cmd.Start()
+			if err != nil {
+				return err
+			}
+
+			stdin.Write([]byte(
+				strings.Join(requestedSessions(cCtx), "\n"),
+			))
+			err = stdin.Close()
+			if err != nil {
+				return err
+			}
+
+			return cmd.Wait()
+		},
+	}
+}
+
+func requestedSessions(cCtx *cli.Context) []string {
+	var sessions []string
+	hasFlags := cCtx.Bool("tmux") || cCtx.Bool("zoxide")
+
+	if !hasFlags || cCtx.Bool("tmux") {
+		tmuxSessions, err := tmux.Sessions()
+		if err != nil {
+			fmt.Println("Error:", err)
+			os.Exit(1)
+		}
+		sessions = append(sessions, tmuxSessions...)
+	}
+
+	if !hasFlags || cCtx.Bool("zoxide") {
+		dirs, err := zoxide.Dirs()
+		if err != nil {
+			fmt.Println("Error:", err)
+			os.Exit(1)
+		}
+		sessions = append(sessions, dirs...)
+	}
+	return sessions
 }
