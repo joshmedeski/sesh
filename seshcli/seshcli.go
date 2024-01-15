1c326cd7a86b1e3ccbe3cf14ff059f1464825bc5
Josh Medeski <josh.medeski@gmail.com>
2024-01-15 12:08:43 -0600

    feat: include version

--- seshcli/seshcli.go
+++ seshcli/seshcli.go
@@ -6,10 +6,13 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
+var version = "dev"
+
 func App() cli.App {
 	return cli.App{
-		Name:  "sesh",
-		Usage: "Smart session manager for the terminal",
+		Name:    "sesh",
+		Version: version,
+		Usage:   "Smart session manager for the terminal",
 		Commands: []*cli.Command{
 			cmds.List(),
 			cmds.Choose(),
