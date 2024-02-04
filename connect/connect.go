7b283a7fc02f073e82b03bdeb6857dc93bf54d4b
Josh Medeski <josh.medeski@gmail.com>
2024-02-03 22:44:54 -0600

    fix: add determine logic back

--- connect/connect.go
+++ connect/connect.go
@@ -1,12 +1,10 @@
 package connect
 
 import (
-	"errors"
 	"fmt"
-	"os"
-	"path/filepath"
 
 	"github.com/joshmedeski/sesh/config"
+	"github.com/joshmedeski/sesh/session"
 	"github.com/joshmedeski/sesh/tmux"
 	"github.com/joshmedeski/sesh/zoxide"
 )
@@ -17,45 +15,15 @@ func Connect(
 	command string,
 	config *config.Config,
 ) error {
-	var errorStack []error
-	isActiveSession := true
-	s, err := tmux.GetSession(choice)
+	session, err := session.Determine(choice, config)
 	if err != nil {
-		isActiveSession = false
-		errorStack = append(errorStack, err)
-	}
-	if !isActiveSession {
-		p, err := filepath.Abs(choice)
-		if err != nil {
-			errorStack = append(errorStack, err)
-			p = choice
-		}
-		info, err := os.Stat(p)
-		if err != nil {
-			errorStack = append(errorStack, err)
-			return fmt.Errorf(
-				"unable to connect to %q: %w",
-				choice,
-				errors.Join(errorStack...),
-			)
-		}
-		if !info.IsDir() {
-			errorStack = append(
-				errorStack,
-				fmt.Errorf("%q found but is not a directory", p),
-			)
-			return errors.Join(errorStack...)
-		}
-		s = tmux.TmuxSession{
-			Name:     filepath.Base(p),
-			Path:     p,
-			Attached: 0,
-		}
-	}
-
-	if err = zoxide.Add(s.Path); err != nil {
 		return fmt.Errorf("unable to connect to %q: %w", choice, err)
 	}
-
-	return tmux.Connect(s, alwaysSwitch, command, s.Path, config)
+	if err = zoxide.Add(session.Path); err != nil {
+		return fmt.Errorf("unable to connect to %q: %w", choice, err)
+	}
+	return tmux.Connect(tmux.TmuxSession{
+		Name: session.Name,
+		Path: session.Path,
+	}, alwaysSwitch, command, session.Path, config)
 }
