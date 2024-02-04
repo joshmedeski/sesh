5cab34dbe793e04b4e43fef6a514b359e1d2df4a
Mark Feinstein <mark.feinstein@gmail.com>
2024-02-03 16:49:03 -0800

    test(tmux): add test for Run method

--- tmux/tmux_test.go
+++ tmux/tmux_test.go
@@ -1,1 +1,19 @@
 package tmux
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCommand_Run(t *testing.T) {
+	c := Command{
+		execFunc: func(string, []string) (string, error) {
+			return "stub", nil
+		},
+	}
+
+	res, err := c.Run([]string{"arg1", "arg2"})
+	require.NoError(t, err)
+	require.Equal(t, "stub", res)
+}
