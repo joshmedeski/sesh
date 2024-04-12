408ad17a1949bedec383f26446f13bd7677a081e
Josh Medeski <josh.medeski@gmail.com>
2024-04-11 21:53:18 -0500

    feat: create shell package

---
+++ shell/shell.go
@@ -0,0 +1,27 @@
+package shell
+
+import (
+	"strings"
+
+	"github.com/joshmedeski/sesh/execwrap"
+)
+
+type Shell struct {
+	exec execwrap.Exec
+}
+
+func New(exec execwrap.Exec) *Shell {
+	return &Shell{exec: exec}
+}
+
+func (c *Shell) Cmd(cmd string, args ...string) (string, error) {
+	command := c.exec.Command(cmd, args...)
+	output, err := command.CombinedOutput()
+	return string(output), err
+}
+
+func (c *Shell) ListCmd(cmd string, args ...string) ([]string, error) {
+	command := c.exec.Command(cmd, args...)
+	output, err := command.Output()
+	return strings.Split(string(output), "\n"), err
+}
---
+++ shell/shell_test.go
@@ -0,0 +1,46 @@
+package shell
+
+import (
+	"testing"
+
+	"github.com/joshmedeski/sesh/execwrap"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestShellCmd(t *testing.T) {
+	t.Run("run should succeed", func(t *testing.T) {
+		mockExec := &execwrap.MockExec{}
+		mockCmd := new(execwrap.MockExecCmd)
+		shell := &Shell{exec: mockExec}
+		mockCmd.On("CombinedOutput").Return([]byte("hello"), nil)
+
+		mockExec.On("Command", "echo", mock.Anything).Return(mockCmd)
+		out, err := shell.Cmd("echo", "hello")
+		assert.Nil(t, err)
+		assert.Equal(t, "hello", out)
+	})
+}
+
+func TestShellListCmd(t *testing.T) {
+	t.Run("run should succeed", func(t *testing.T) {
+		mockExec := &execwrap.MockExec{}
+		mockCmd := new(execwrap.MockExecCmd)
+		shell := &Shell{exec: mockExec}
+		dirListingActual := []byte(`total 9720
+drwxr-xr-x  17 joshmedeski  staff      544 Apr 11 21:40 ./
+drwxr-xr-x   8 joshmedeski  staff      256 Apr 11 19:05 ../
+-rw-r--r--   1 joshmedeski  staff       53 Apr 11 09:00 .git`)
+		mockCmd.On("Output").Return(dirListingActual, nil)
+		mockExec.On("Command", "ls", mock.Anything).Return(mockCmd)
+		dirListingExpected := []string{
+			"total 9720",
+			"drwxr-xr-x  17 joshmedeski  staff      544 Apr 11 21:40 ./",
+			"drwxr-xr-x   8 joshmedeski  staff      256 Apr 11 19:05 ../",
+			"-rw-r--r--   1 joshmedeski  staff       53 Apr 11 09:00 .git",
+		}
+		list, err := shell.ListCmd("ls", "-la")
+		assert.Nil(t, err)
+		assert.Equal(t, dirListingExpected, list)
+	})
+}
