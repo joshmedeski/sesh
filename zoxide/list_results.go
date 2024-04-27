1a2053ce56db79f05c4f275158dd820bf579817d
Josh Medeski <josh.medeski@gmail.com>
2024-04-27 10:45:38 -0500

    fix: zoxide query to include score and trim empty lines

--- zoxide/list_results.go
+++ zoxide/list_results.go
@@ -8,16 +8,23 @@ import (
 )
 
 func (z *RealZoxide) ListResults() ([]model.ZoxideResult, error) {
-	list, err := z.shell.ListCmd("zoxide", "query", "-l")
+	list, err := z.shell.ListCmd("zoxide", "query", "--list", "--score")
 	if err != nil {
 		return nil, err
 	}
 	results := make([]model.ZoxideResult, 0, len(list))
 	for _, result := range list {
+		if result == "" {
+			break
+		}
 		trimmedResult := strings.TrimSpace(result)
 		fields := strings.SplitN(trimmedResult, " ", 2)
+		score, err := convert.StringToFloat(fields[0])
+		if err != nil {
+			return nil, err
+		}
 		results = append(results, model.ZoxideResult{
-			Score: convert.StringToFloat(fields[0]),
+			Score: score,
 			Path:  fields[1],
 		})
 	}
