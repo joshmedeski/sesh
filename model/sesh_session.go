220acf9d05f397ee2cfb613af39cf9857bcde717
Josh Medeski <josh.medeski@gmail.com>
2024-05-01 08:39:44 -0500

    chore: simplify type definitions with wrap

--- model/sesh_session.go
+++ model/sesh_session.go
@@ -1,17 +1,19 @@
 package model
 
-type SeshSession struct {
-	Src  string // The source of the session (config, tmux, zoxide)
-	Name string // The display name
-	Path string // The absolute directory path
+type (
+	SeshSession struct {
+		Src  string // The source of the session (config, tmux, zoxide)
+		Name string // The display name
+		Path string // The absolute directory path
 
-	Attached int     // Whether the session is currently attached
-	Windows  int     // The number of windows in the session
-	Score    float64 // The score of the session (from Zoxide)
-}
+		Attached int     // Whether the session is currently attached
+		Windows  int     // The number of windows in the session
+		Score    float64 // The score of the session (from Zoxide)
+	}
 
-type SeshSrcs struct {
-	Config bool
-	Tmux   bool
-	Zoxide bool
-}
+	SeshSrcs struct {
+		Config bool
+		Tmux   bool
+		Zoxide bool
+	}
+)
