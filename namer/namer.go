4cce7555b90282029dcc5d7decdaa392b6c0ad7d
Josh Medeski <josh.medeski@gmail.com>
2024-07-01 09:27:52 -0500

    feat: convert to valid name for tmux

--- namer/namer.go
+++ namer/namer.go
@@ -24,13 +24,21 @@ func NewNamer(pathwrap pathwrap.Path, git git.Git) Namer {
 	}
 }
 
+func convertToValidName(name string) string {
+	validName := strings.ReplaceAll(name, ".", "_")
+	validName = strings.ReplaceAll(validName, ":", "_")
+	return validName
+}
+
 func (n *RealNamer) FromPath(path string) (string, error) {
+	var name string
 	isGit, topLevelDir, _ := n.git.ShowTopLevel(path)
 	if isGit && topLevelDir != "" {
 		relativePath := strings.TrimPrefix(path, topLevelDir)
 		baseDir := n.pathwrap.Base(topLevelDir)
-		name := baseDir + relativePath
-		return name, nil
+		name = baseDir + relativePath
+	} else {
+		name = n.pathwrap.Base(path)
 	}
-	return n.pathwrap.Base(path), nil
+	return convertToValidName(name), nil
 }
