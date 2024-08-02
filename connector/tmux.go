7bdcf75c3ed6c99183ccfca8cec5b12e0ecdc8b3
Josh Medeski <josh.medeski@gmail.com>
2024-08-01 19:05:56 -0500

    chore: remove todo

--- connector/tmux.go
+++ connector/tmux.go
@@ -3,7 +3,6 @@ package connector
 import "github.com/joshmedeski/sesh/model"
 
 func tmuxStrategy(c *RealConnector, name string) (model.Connection, error) {
-	// TODO: find by name or by path?
 	session, exists := c.lister.FindTmuxSession(name)
 	if !exists {
 		return model.Connection{Found: false}, nil
