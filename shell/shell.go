95793f8cfde6982acf37af59e438979cb639b6d2
Josh Medeski <josh.medeski@gmail.com>
2024-08-22 20:24:39 -0500

    fix: build command to start and wait

--- shell/shell.go
+++ shell/shell.go
@@ -1,6 +1,9 @@
 package shell
 
 import (
+	"bytes"
+	"os"
+	"os/exec"
 	"strings"
 
 	"github.com/joshmedeski/sesh/execwrap"
@@ -19,11 +22,29 @@ func NewShell(exec execwrap.Exec) Shell {
 	return &RealShell{exec}
 }
 
-func (c *RealShell) Cmd(cmd string, arg ...string) (string, error) {
-	command := c.exec.Command(cmd, arg...)
-	output, err := command.CombinedOutput()
-	trimmedOutput := strings.TrimSuffix(string(output), "\n")
-	return trimmedOutput, err
+func (c *RealShell) Cmd(cmd string, args ...string) (string, error) {
+	foundCmd, err := c.exec.LookPath(cmd)
+	if err != nil {
+		return "", err
+	}
+	var stdout, stderr bytes.Buffer
+	command := exec.Command(foundCmd, args...)
+	command.Stdin = os.Stdin
+	command.Stdout = &stdout
+	command.Stderr = os.Stderr
+	command.Stderr = &stderr
+	if err := command.Start(); err != nil {
+		return "", err
+	}
+	if err := command.Wait(); err != nil {
+		errString := strings.TrimSpace(stderr.String())
+		if strings.HasPrefix(errString, "no server running on") {
+			return "", nil
+		}
+		return "", err
+	}
+	trimmedOutput := strings.TrimSuffix(string(stdout.String()), "\n")
+	return trimmedOutput, nil
 }
 
 func (c *RealShell) ListCmd(cmd string, arg ...string) ([]string, error) {
