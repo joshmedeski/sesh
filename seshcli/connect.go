05c428d58c3c961a87163801679efa0c99c35f50
Josh Medeski <josh.medeski@gmail.com>
2024-08-22 20:23:02 -0500

    feat: update mocks

--- seshcli/connect.go
+++ seshcli/connect.go
@@ -10,6 +10,36 @@ import (
 	cli "github.com/urfave/cli/v2"
 )
 
+//	func tmuxCmd(args []string) (string, error) {
+//		tmux, err := exec.LookPath("tmux")
+//		if err != nil {
+//			return "", err
+//		}
+//		var stdout, stderr bytes.Buffer
+//		cmd := exec.Command(tmux, args...)
+//		cmd.Stdin = os.Stdin
+//		cmd.Stdout = &stdout
+//		cmd.Stderr = os.Stderr
+//		cmd.Stderr = &stderr
+//		if err := cmd.Start(); err != nil {
+//			return "", err
+//		}
+//		if err := cmd.Wait(); err != nil {
+//			errString := strings.TrimSpace(stderr.String())
+//			if strings.HasPrefix(errString, "no server running on") {
+//				return "", nil
+//			}
+//			return "", err
+//		}
+//		return stdout.String(), nil
+//	}
+//
+//	func attachSession(session string) error {
+//		if _, err := tmuxCmd([]string{"attach", "-t", session}); err != nil {
+//			return err
+//		}
+//		return nil
+//	}
 func Connect(c connector.Connector, i icon.Icon) *cli.Command {
 	return &cli.Command{
 		Name:                   "connect",
@@ -47,6 +77,9 @@ func Connect(c connector.Connector, i icon.Icon) *cli.Command {
 				fmt.Println(connection)
 				return nil
 			}
+
+			// attachSession("sesh/v2")
+			// return nil
 		},
 	}
 }
