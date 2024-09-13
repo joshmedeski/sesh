845298f5a221c77018c332fc4e7ee7bdb514bc04
Josh Medeski <josh.medeski@gmail.com>
2024-09-12 19:39:39 -0500

    feat: add a last cli command

---
+++ seshcli/last.go
@@ -0,0 +1,26 @@
+package seshcli
+
+import (
+	"github.com/joshmedeski/sesh/lister"
+	"github.com/joshmedeski/sesh/tmux"
+	cli "github.com/urfave/cli/v2"
+)
+
+func Last(l lister.Lister, t tmux.Tmux) *cli.Command {
+	return &cli.Command{
+		Name:                   "last",
+		Aliases:                []string{"L"},
+		Usage:                  "Connect to the last tmux session",
+		UseShortOptionHandling: true,
+		Flags:                  []cli.Flag{},
+		Action: func(cCtx *cli.Context) error {
+			lastSession, exists := l.GetLastTmuxSession()
+			if !exists {
+				// TODO: silently fail?
+				return cli.Exit("No last session found", 1)
+			}
+			t.SwitchClient(lastSession.Name)
+			return nil
+		},
+	}
+}
