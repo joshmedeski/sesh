dc7b5550bd35b6fac71511ea9f169f99f2cd8571
Josh Medeski <josh.medeski@gmail.com>
2024-09-19 20:29:27 -0500

    chore: update mockery

--- tmuxinator/mock_Tmuxinator.go
+++ tmuxinator/mock_Tmuxinator.go
@@ -20,12 +20,12 @@ func (_m *MockTmuxinator) EXPECT() *MockTmuxinator_Expecter {
 	return &MockTmuxinator_Expecter{mock: &_m.Mock}
 }
 
-// ListConfigs provides a mock function with given fields:
-func (_m *MockTmuxinator) ListConfigs() ([]*model.TmuxinatorConfig, error) {
+// List provides a mock function with given fields:
+func (_m *MockTmuxinator) List() ([]*model.TmuxinatorConfig, error) {
 	ret := _m.Called()
 
 	if len(ret) == 0 {
-		panic("no return value specified for ListConfigs")
+		panic("no return value specified for List")
 	}
 
 	var r0 []*model.TmuxinatorConfig
@@ -50,29 +50,29 @@ func (_m *MockTmuxinator) ListConfigs() ([]*model.TmuxinatorConfig, error) {
 	return r0, r1
 }
 
-// MockTmuxinator_ListConfigs_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'ListConfigs'
-type MockTmuxinator_ListConfigs_Call struct {
+// MockTmuxinator_List_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'List'
+type MockTmuxinator_List_Call struct {
 	*mock.Call
 }
 
-// ListConfigs is a helper method to define mock.On call
-func (_e *MockTmuxinator_Expecter) ListConfigs() *MockTmuxinator_ListConfigs_Call {
-	return &MockTmuxinator_ListConfigs_Call{Call: _e.mock.On("ListConfigs")}
+// List is a helper method to define mock.On call
+func (_e *MockTmuxinator_Expecter) List() *MockTmuxinator_List_Call {
+	return &MockTmuxinator_List_Call{Call: _e.mock.On("List")}
 }
 
-func (_c *MockTmuxinator_ListConfigs_Call) Run(run func()) *MockTmuxinator_ListConfigs_Call {
+func (_c *MockTmuxinator_List_Call) Run(run func()) *MockTmuxinator_List_Call {
 	_c.Call.Run(func(args mock.Arguments) {
 		run()
 	})
 	return _c
 }
 
-func (_c *MockTmuxinator_ListConfigs_Call) Return(_a0 []*model.TmuxinatorConfig, _a1 error) *MockTmuxinator_ListConfigs_Call {
+func (_c *MockTmuxinator_List_Call) Return(_a0 []*model.TmuxinatorConfig, _a1 error) *MockTmuxinator_List_Call {
 	_c.Call.Return(_a0, _a1)
 	return _c
 }
 
-func (_c *MockTmuxinator_ListConfigs_Call) RunAndReturn(run func() ([]*model.TmuxinatorConfig, error)) *MockTmuxinator_ListConfigs_Call {
+func (_c *MockTmuxinator_List_Call) RunAndReturn(run func() ([]*model.TmuxinatorConfig, error)) *MockTmuxinator_List_Call {
 	_c.Call.Return(run)
 	return _c
 }
