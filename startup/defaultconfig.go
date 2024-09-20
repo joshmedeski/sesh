c0052cd2522b4e7eae322152cb0ff51e6bf1d2ae
Martin Norberg <nobbehhh@gmail.com>
2024-09-20 22:27:21 +0200

    feat: disable_startup_command for sessions

--- startup/defaultconfig.go
+++ startup/defaultconfig.go
@@ -3,9 +3,14 @@ package startup
 import "github.com/joshmedeski/sesh/model"
 
 func defaultConfigStrategy(s *RealStartup, session model.SeshSession) (string, error) {
+	if session.DisableStartupCommand {
+		return "", nil
+	}
+
 	defaultConfig := s.config.DefaultSessionConfig
 	if defaultConfig.StartupCommand != "" {
 		return defaultConfig.StartupCommand, nil
 	}
+
 	return "", nil
 }
