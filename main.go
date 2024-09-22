b4f376a036df24b6803ecf7f116e305471474e8d
Andres Court <andres@andrescourt.com>
2024-09-22 11:05:25 -0500

    feat: Configure slog levels using ENV variable

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"log"
+	"log/slog"
 	"os"
+	"strings"
 
 	"github.com/joshmedeski/sesh/seshcli"
 )
@@ -10,8 +12,32 @@ import (
 var version = "dev"
 
 func main() {
+  slog.Debug("Debug")
+  slog.Info("Information")
+  slog.Warn("Warning")
+  slog.Error("Error")
+
 	app := seshcli.App(version)
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
+}
+func init() {
+	env := os.Getenv("ENV")
+
+  handlerOptions := &slog.HandlerOptions{}
+
+  switch strings.ToLower(env) {
+  case "debug":
+    handlerOptions.Level = slog.LevelDebug
+  case "info":
+    handlerOptions.Level = slog.LevelInfo
+  case "error":
+    handlerOptions.Level = slog.LevelError
+  default:
+    handlerOptions.Level = slog.LevelWarn
+  }
+
+  loggerHandler := slog.NewJSONHandler(os.Stdout, handlerOptions)
+  slog.SetDefault(slog.New(loggerHandler))
 }
