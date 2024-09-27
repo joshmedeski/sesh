eb0adc17427fdc68c46c70decdfad06de9ecbc8f
Josh Medeski <josh.medeski@gmail.com>
2024-09-26 19:43:32 -0500

    feat: create RootDir function

--- dir/dir.go
+++ dir/dir.go
@@ -1,21 +1,26 @@
 package dir
 
 import (
+	"strings"
+
+	"github.com/joshmedeski/sesh/git"
 	"github.com/joshmedeski/sesh/oswrap"
 	"github.com/joshmedeski/sesh/pathwrap"
 )
 
 type Dir interface {
 	Dir(name string) (isDir bool, absPath string)
+	RootDir(name string) (hasRootDir bool, absPath string)
 }
 
 type RealDir struct {
 	os   oswrap.Os
+	git  git.Git
 	path pathwrap.Path
 }
 
-func NewDir(os oswrap.Os, path pathwrap.Path) Dir {
-	return &RealDir{os, path}
+func NewDir(os oswrap.Os, git git.Git, path pathwrap.Path) Dir {
+	return &RealDir{os, git, path}
 }
 
 func (d *RealDir) Dir(path string) (isDir bool, absPath string) {
@@ -33,4 +38,20 @@ func (d *RealDir) Dir(path string) (isDir bool, absPath string) {
 	}
 
 	return true, absPath
+}
+
+func (d *RealDir) RootDir(path string) (hasRootDir bool, absPath string) {
+	isGit, commonDir, _ := d.git.GitCommonDir(path)
+	if isGit && strings.HasSuffix(commonDir, "/.bare") {
+		topLevelDir := strings.TrimSuffix(commonDir, "/.bare")
+		relativePath := strings.TrimPrefix(path, topLevelDir)
+		firstDir := strings.Split(relativePath, string("/"))[1]
+		name, err := d.path.Abs(topLevelDir + "/" + firstDir)
+		if err != nil {
+			return false, ""
+		}
+		return true, name
+	} else {
+		return false, ""
+	}
 }
