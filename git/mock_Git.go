bf4b4cc235d709c4471fd46bacb3db72765cda81
Jesse Leite <jesseleite@gmail.com>
2024-11-05 18:22:06 -0500

    Update mocks.

--- git/mock_Git.go
+++ git/mock_Git.go
@@ -136,6 +136,69 @@ func (_c *MockGit_GitCommonDir_Call) RunAndReturn(run func(string) (bool, string
 	return _c
 }
 
+// GitMainWorktree provides a mock function with given fields: name
+func (_m *MockGit) GitMainWorktree(name string) (bool, string, error) {
+	ret := _m.Called(name)
+
+	if len(ret) == 0 {
+		panic("no return value specified for GitMainWorktree")
+	}
+
+	var r0 bool
+	var r1 string
+	var r2 error
+	if rf, ok := ret.Get(0).(func(string) (bool, string, error)); ok {
+		return rf(name)
+	}
+	if rf, ok := ret.Get(0).(func(string) bool); ok {
+		r0 = rf(name)
+	} else {
+		r0 = ret.Get(0).(bool)
+	}
+
+	if rf, ok := ret.Get(1).(func(string) string); ok {
+		r1 = rf(name)
+	} else {
+		r1 = ret.Get(1).(string)
+	}
+
+	if rf, ok := ret.Get(2).(func(string) error); ok {
+		r2 = rf(name)
+	} else {
+		r2 = ret.Error(2)
+	}
+
+	return r0, r1, r2
+}
+
+// MockGit_GitMainWorktree_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'GitMainWorktree'
+type MockGit_GitMainWorktree_Call struct {
+	*mock.Call
+}
+
+// GitMainWorktree is a helper method to define mock.On call
+//   - name string
+func (_e *MockGit_Expecter) GitMainWorktree(name interface{}) *MockGit_GitMainWorktree_Call {
+	return &MockGit_GitMainWorktree_Call{Call: _e.mock.On("GitMainWorktree", name)}
+}
+
+func (_c *MockGit_GitMainWorktree_Call) Run(run func(name string)) *MockGit_GitMainWorktree_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		run(args[0].(string))
+	})
+	return _c
+}
+
+func (_c *MockGit_GitMainWorktree_Call) Return(_a0 bool, _a1 string, _a2 error) *MockGit_GitMainWorktree_Call {
+	_c.Call.Return(_a0, _a1, _a2)
+	return _c
+}
+
+func (_c *MockGit_GitMainWorktree_Call) RunAndReturn(run func(string) (bool, string, error)) *MockGit_GitMainWorktree_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
 // ShowTopLevel provides a mock function with given fields: name
 func (_m *MockGit) ShowTopLevel(name string) (bool, string, error) {
 	ret := _m.Called(name)
