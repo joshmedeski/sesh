231f1a7b1fb4778740a4fb872bdef1b6d6a1020b
Jesse Leite <jesseleite@gmail.com>
2024-11-05 19:23:09 -0500

    This was never used/referenced anywhere. (And we’re about to refactor some of this anyway.)

--- namer/git.go
+++ namer/git.go
@@ -5,20 +5,6 @@ import (
 	"strings"
 )
 
-func gitBareRootName(n *RealNamer, path string) (string, error) {
-	isGit, commonDir, _ := n.git.GitCommonDir(path)
-	if isGit && strings.HasSuffix(commonDir, "/.bare") {
-		topLevelDir := strings.TrimSuffix(commonDir, "/.bare")
-		name, err := n.home.ShortenHome(topLevelDir)
-		if err != nil {
-			return "", fmt.Errorf("couldn't shorten path: %q", err)
-		}
-		return name, nil
-	} else {
-		return "", nil
-	}
-}
-
 // Gets the name from a git bare repository
 func gitBareName(n *RealNamer, path string) (string, error) {
 	var name string
