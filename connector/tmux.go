128647ae63b9fa9eb9fce89c72efc2d7fe8fc1d6
Joe Dang <mrjoedang@gmail.com>
2025-09-27 12:17:13 -0600

    Fix command flag

--- connector/tmux.go
+++ connector/tmux.go
@@ -18,7 +18,11 @@ func tmuxStrategy(c *RealConnector, name string) (model.Connection, error) {
 func connectToTmux(c *RealConnector, connection model.Connection, opts model.ConnectOpts) (string, error) {
 	if connection.New {
 		c.tmux.NewSession(connection.Session.Name, connection.Session.Path)
-		c.startup.Exec(connection.Session)
+		if opts.Command != "" {
+			c.tmux.SendKeys(connection.Session.Name, opts.Command)
+		} else {
+			c.startup.Exec(connection.Session)
+		}
 	}
 	return c.tmux.SwitchOrAttach(connection.Session.Name, opts)
 }
